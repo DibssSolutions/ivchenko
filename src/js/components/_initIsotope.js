38deadccd45ca42d58bd81569e7b6c1de2ecdbcf
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-01-29 19:19:10 +0200

    filter

--- src/js/components/_initIsotope.js
+++ src/js/components/_initIsotope.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-var Isotope = require("isotope-layout");
+import LazyLoad from 'vanilla-lazyload';
+import Isotope from 'isotope-layout';
+import imagesLoaded from 'imagesloaded';
+import { INIT } from '../constants';
 
-var iso = new Isotope(".js-filter", {
-  itemSelector: ".work",
+var iso;
+
+var myLazyLoad = new LazyLoad({
+  elements_selector: '[data-masonry-lazy]',
+  // treshold: 1000,
+  callback_finish: () => iso.layout()
+});
+
+myLazyLoad.loadAll();
+
+iso = new Isotope('.js-masonry', {
+  itemSelector: '.js-masonry-item',
   masonry: {
-    columnWidth: ".grid-sizer",
-    gutter: '.gutter-sizer'
-    // columnWidth: 300
+    columnWidth: '.js-masonry-width',
+    gutter: '.js-masonry-gutter'
   }
 });
+
+iso.on('layoutComplete', () => $('.js-masonry').addClass(INIT));
+
+$('[data-filter]').click(function() {
+  /*
+  FILTERING
+  */
+  var filterValue = $(this).attr('data-filter');
+  iso.arrange({ filter: filterValue });
+});
+
+/*
+  CHANGING FILTER BUTTON CLASS
+  */
+// $(".filter__buttons")
+//   .find(".is-active")
+//   .removeClass("is-active");
+// $(this).addClass("is-active");
