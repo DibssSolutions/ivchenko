fd3e9d27c17ca15462f4e23c53ab8eaa25cf7a82
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-02-04 15:17:14 +0200

    fixes v1.0

--- src/js/components/_mainLoader.js
+++ src/js/components/_mainLoader.js
@@ -1,11 +1,14 @@
 import { BODY, DOC, HTML, LOADED, HIDDEN, OVERFLOW_HIDDEN } from '../constants';
-const container = $('.js-main-loader');
-HTML.addClass(OVERFLOW_HIDDEN);
-DOC.ready(() => { 
-  BODY.addClass(LOADED);
-  setTimeout(() => container.addClass(HIDDEN), 3000);
-  setTimeout(() => {
-  	container.remove();
-  	HTML.removeClass(OVERFLOW_HIDDEN);
-  }, 5000);
-});
+
+export default function mainLoader() {
+  DOC.ready(() => {
+    const container = $('.js-main-loader');
+    HTML.addClass(OVERFLOW_HIDDEN);
+    BODY.addClass(LOADED);
+    setTimeout(() => container.addClass(HIDDEN), 3000);
+    setTimeout(() => {
+      container.remove();
+      HTML.removeClass(OVERFLOW_HIDDEN);
+    }, 5000);
+  });
+}
