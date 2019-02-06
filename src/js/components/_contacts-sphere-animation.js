4333e42a0537b56f98c9d4ee780f7868fc609beb
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-02-06 18:13:22 +0200

    pixel perf all

---
+++ src/js/components/_contacts-sphere-animation.js
@@ -0,0 +1,21 @@
+import { TweenLite } from 'gsap';
+import SCROLLTRIGGER from './_scrollTrigger';
+
+if ($('.js-contacts-sphere')[0]) {
+  const sphere = $('.js-contacts-sphere');
+  const top = sphere.offset().top;
+  const bottom = $('.js-contacts-sphere-bottom-position').offset().top;
+  const height = sphere.height() / 2;
+  const yTransform = bottom - top - height + 30;
+
+  const sphereAnim = new TimelineMax({ paused: true });
+  sphereAnim.to(sphere, 3, {
+    y: yTransform,
+    ease: Power4.easeOut
+  });
+
+  new SCROLLTRIGGER({
+    container: $('[data-contacts-sphere-trigger]'),
+    onStart: () => sphereAnim.play()
+  });
+}
