7b25cfb0a5c6d6f0f6bb201a2b0ba1b5b680b1e3
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-02-08 12:11:26 +0200

    v 1.0 ready

--- js/0.js
+++ js/0.js
@@ -1,1 +1,1 @@
-webpackJsonp([0],{59:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=59}});
+webpackJsonp([0],{60:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=60}});
