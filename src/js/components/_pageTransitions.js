c1b2c5fea848012a9b8828d5911f34d453b8e619
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-02-11 15:42:53 +0200

    fixes v3.0

--- src/js/components/_pageTransitions.js
+++ src/js/components/_pageTransitions.js
@@ -3,6 +3,10 @@ import { ACTIVE, OVERFLOW_HIDDEN, HTML, LOADED, BODY } from '../constants';
 $('.js-page-link').each((index, el) => {
   var link = $(el);
   link.on('click', e => {
+    if (link.hasClass('js-current-link')) {
+      // return false;
+      console.log(`Current link is ${window.href}`);
+    }
     e.preventDefault();
     $('.js-loader').addClass(ACTIVE);
     setTimeout(() => (window.location = link[0].href), 500);
