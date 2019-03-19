e811440781c4cd105c1e75e7d0917c5b7e00a7b7
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-03-19 13:12:59 +0200

    push

--- src/js/components/_video.js
+++ src/js/components/_video.js
@@ -2,9 +2,18 @@ const videos = $('.js-video-wrapper');
 
 videos.each((index, el) => {
   const video = $(el).find('.js-video')[0];
-  const btn = $(el).find('.js-video-play-btn');
+  const playBtn = $(el).find('.js-video-play-btn');
+  const pauseBtn = $(el).find('.js-video-pause-btn');
 
-  btn.on('click', () => {
+  playBtn.on('click', () => {
     video.play();
+    $(el).addClass('is-playing');
+    $(el).removeClass('is-paused');
+  });
+
+  pauseBtn.on('click', () => {
+    video.pause();
+    $(el).addClass('is-paused');
+    $(el).removeClass('is-playing');
   });
 });
